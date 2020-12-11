ab05ea5a46ff3dae226b30b63be1592e54239e77
Roberto D'Auria <roberto@measurementlab.net>
2020-12-11 15:21:26 +0100

    Add SchoolID field to the database schema.

--- internal/model/measurement.go
+++ internal/model/measurement.go
@@ -16,6 +16,9 @@ type Measurement struct {
 	// BrowserID is a unique identifier for this Measurement's uploader.
 	BrowserID string `sql:",notnull" validate:"required"`
 
+	// SchoolID identifies the school sending this Measurement.
+	SchoolID string `sql:",notnull" validate:"required"`
+
 	// DeviceType is a free-form string identifying the device type.
 	DeviceType string
 
